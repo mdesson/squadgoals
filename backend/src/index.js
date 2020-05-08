20cd3609c367363ad32337f92826e2537ff45292
mdesson <michael.desson@gmail.com>
2020-05-08 09:50:04 -0400

    Ran prettier, deleted extraneous directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,15 @@
-import 'dotenv/config'
-import express from 'express'
+import "dotenv/config";
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-	return res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
 
 app.listen(process.env.PORT, () => {
-	console.log(`frontend is listening on port ${process.env.PORT}`)
-})
+  console.log(`frontend is listening on port ${process.env.PORT}`);
+});
