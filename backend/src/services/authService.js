9df442514413b9561e5168ee61a9e198b983cd45
Michael Desson <michael.desson@gmail.com>
2020-05-26 10:59:25 -0400

    Created AuthService to handle login/singUps

---
+++ backend/src/services/authService.js
@@ -0,0 +1,58 @@
+import * as argon2 from "argon2";
+import * as jwt from "jsonwebtoken";
+import { randomBytes } from "crypto";
+import { models } from "../models";
+
+const AuthService = {
+  async SignUp(firstName, lastName, email, aspirationalMessage, password) {
+    // Create the user
+    const userRecord = await models.User.create({
+      firstName,
+      lastName,
+      email,
+      aspirationalMessage,
+    });
+
+    // Generate salt and use it to hash password
+    const salt = randomBytes(32);
+    const passwordHashed = await argon2.hash(password, { salt });
+
+    // Generate auth
+    const userAuth = await models.Auth.create({
+      userId: userRecord.id,
+      hash: passwordHashed,
+      salt: salt.toString("hex"),
+    });
+
+    const token = generateJWT(userRecord);
+
+    return { token, user: userRecord };
+  },
+
+  async Login(email, password) {
+    const user = await models.User.findByEmail(email);
+    const userAuth = models.Auth.findByUserId(user.id);
+    const passwordValid = await argon2.verify(userAuth.hash, password);
+
+    // No user, return error message
+    // TODO: Stanardize returning errors
+    if (!user) return { error: "Invalid username or password" };
+
+    // Invalid password, return error message
+    // TODO: Stanardize returning errors
+    const passwordValid = await argon2.verify(userAuth.hash, password);
+    if (!user) return { error: "Invalid username or password" };
+  },
+};
+
+const generateJWT = (user) => {
+  return jwt.sign(
+    {
+      data: user,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: process.env.JWT_EXPIRES_IN }
+  );
+};
+
+export default AuthService;
