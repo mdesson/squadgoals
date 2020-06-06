3dffe2d8475b8e335717b30e9472fdf293772252
seanheinrichs <seanheinrichs@gmail.com>
2020-06-06 17:56:22 -0400

    implement add squadmember, delte squadmember and fetch all squadmembers

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,16 +1,19 @@
 const User = require('./user');
 const Auth = require('./auth');
 const Squad = require('./squad');
+const SquadMember = require('./squadMember');
 
 const models = {
   User: User,
   Auth: Auth,
   Squad: Squad,
+  SquadMember: SquadMember,
 };
 
 // Set Database Relations
 Auth.belongsTo(User);
 User.hasMany(Squad, { constraints: true, onDelete: 'CASCADE' });
+Squad.belongsToMany(User, { through: SquadMember });
 
 // Associate foreign keys
 Object.keys(models).forEach((key) => {
